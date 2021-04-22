4be8a5a57cbbf325bf08d017d16fb763b5e683b0
Reshul <53306036+resel143@users.noreply.github.com>
2021-04-22 12:05:31 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+var audio = new Audio();
+
+function stop(){
+    audio.pause();
+}
+function lytlm(){
+    audio = new Audio('lytlm.mp3');
+    audio.play();
+}
+function icge(){
+    audio = new Audio('icge.mp3');
+    audio.play();
+
+}
+function iwytk(){
+    audio = new Audio('iwytk.mp3');
+    audio.play();
+
+}
+function n(){
+    audio = new Audio('n.mp3');
+    audio.play();
+
+}
+function rare(){
+    audio = new Audio('rate.mp3');
+    audio.play();
+
+}
+function sdts(){
+    audio = new Audio('sdts.mp3');
+    audio.play();
+}
